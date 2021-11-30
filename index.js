abcbacb73d82f3724851331d5d0aed3a05bfcdf8
Kyler <kyler.b@live.com>
2021-11-30 09:16:18 -0600

    commit tree c14924e6490a99cb6e28dc17aa369246e8079423

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Polygon{
+    constructor(sides){
+        this.sides = sides
+    }
+    get countSides(){
+        return this.sides.length
+    }
+    get perimeter(){
+        return this.sides.reduce(function(prev, current){
+            return prev + current
+        })
+    }
+}
+class Triangle extends Polygon{
+    get isValid(){
+        for(const side of this.sides){
+            if((this.perimeter-side) < side) return false
+        }
+        return true
+    }
+}
+class Square extends Polygon{
+    get isValid(){
+        let s = this.sides
+        return ((
+            s[0] === s[1] && 
+            s[0] === s[2] && 
+            s[0] === s[3]))
+    }
+    get area(){return this.sides[0]**2}
+}
